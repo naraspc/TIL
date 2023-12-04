73b463ebe9f08e302b54bb747fc139dcda62a119
naraspc <wodnrtkfka@naver.com>
2023-12-04 23:40:27 +0900

    test

--- src/main/java/com/example/til/controller/Example1Controller.java
+++ src/main/java/com/example/til/controller/Example1Controller.java
@@ -4,5 +4,8 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class Example1Controller {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 
 }
